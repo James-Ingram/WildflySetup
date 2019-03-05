37334c71791acb2f27809a42cdbfad896540cb17
smurfjames101 <smurfjames@gmail.com>
2019-03-05 13:53:31 +0000

    Update bobbeth.sh

--- bobbeth.sh
+++ bobbeth.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install figlet -y
+sudo apt-get install sl
 echo "figlet Installed"
 figlet "Sir, Sir?! Sir Bobbeth?!" >  ~/.bobbeth
 echo "Bobbeth Created"
