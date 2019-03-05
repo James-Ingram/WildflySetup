093664b1c048f62f5e1a9e7f2d24d07b03cff802
smurfjames101 <smurfjames@gmail.com>
2019-03-05 15:36:35 +0000

    Fixed Escape Characters

--- bobbeth.sh
+++ bobbeth.sh
@@ -5,7 +5,7 @@ sudo apt-get install sl
 echo "figlet Installed"
 figlet "Sir, Sir?! Sir Bobbeth?!" >  ~/.bobbeth
 echo "Bobbeth Created"
-alias h='figlet "Hail, Sir Bobbeth!"' >> ~/.bashrc
-alias hr='figlet "Hath Thy Man Been Slighted?"' >> ~./bashrc
+echo "alias h='figlet \"Hail, Sir Bobbeth!\"'" >> ~/.bashrc
+echo "alias hr='figlet \"Hath Thy Man Been Slighted?\"'" >> ~/.bashrc
 echo "cat ~/.bobbeth" >> ~/.bashrc
 exit
