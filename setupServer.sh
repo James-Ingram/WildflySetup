88155adea99b8f00b0caec8f4d161af8e3f97dbb
smurfjames101 <smurfjames@gmail.com>
2019-03-08 10:17:06 +0000

    Fixed New Path Name

--- setupServer.sh
+++ setupServer.sh
@@ -7,6 +7,6 @@ sudo apt-get install npm -y
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
 rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp bobbeth/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
+cp WildflySetup/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
 sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
 
