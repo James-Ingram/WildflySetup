3c764ef7025c2d60dfd95b2b4231422f64d6a087
smurfjames101 <smurfjames@gmail.com>
2019-03-08 10:15:00 +0000

    No Figlet, starts Wildfly background process

--- setupServer.sh
+++ setupServer.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install figlet -y
 sudo apt-get install unzip
-figlet "Unzip Installed."
 sudo apt-get install default-jre -y
-figlet "Jave JRE Installed."
 sudo apt-get install node.js -y
-figlet "Node.js Installed."
 sudo apt-get install npm -y
-figlet "NPM Installed."
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
-figlet "Wildfly Unzipped."
 rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
 cp bobbeth/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
-figlet "Swapped Standalone.xml."
+sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
+
